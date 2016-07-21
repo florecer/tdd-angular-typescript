51b7b0cf9339e9091cd93d6721a289bf0bd2b13b
faunus <faunotv@gmail.com>
2016-07-20 19:30:06 -0500

    + wallaby

---
+++ wallaby.angular.js
@@ -0,0 +1,46 @@
+module.exports = function () {
+    return {
+        testFramework: 'mocha',
+        debug: true,
+        files: [
+            {
+                pattern: 'node_modules/phantomjs-polyfill/bind-polyfill.js', instrument: false
+            },
+            { 
+                pattern: 'node_modules/angular/angular.js', 
+                instrument: false
+            },
+            { 
+                pattern: 'node_modules/angular-mocks/angular-mocks.js', instrument: false 
+            },
+            { 
+                pattern: "node_modules/bardjs/dist/bard.min.js", 
+                instrument: false 
+            },
+            { 
+                pattern: 'node_modules/chai/chai.js', 
+                instrument: false 
+            },
+            { 
+                pattern: 'node_modules/sinon/pkg/sinon.js', 
+                instrument: false 
+            },
+            { 
+                pattern: 'node_modules/sinon-chai/lib/sinon-chai.js', 
+                instrument: false 
+            },
+            { 
+                // pattern: 'src/*.js',
+                pattern: 'src/*.ts', 
+                load: false 
+            }
+        ],
+        tests: [
+            {
+                // pattern: 'test/*.spec.js', 
+                pattern: 'test/*.spec.ts', 
+                load: false
+            }
+        ]
+    };
+};
